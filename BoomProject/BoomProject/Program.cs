967c6053e92cc7f610dd807a964771f87ce472d2
nguyenhuuhoa <nguyenhuuhoa1998@gmail.com>
2019-07-03 02:32:57 +0700

    Revert "start screen"
    This reverts commit c83f486a9786a04173d94bc9246d4b07a6fcb569.

--- BoomProject/BoomProject/Program.cs
+++ BoomProject/BoomProject/Program.cs
@@ -16,11 +16,7 @@ namespace BoomProject
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-<<<<<<< Updated upstream
             Application.Run(new Form1());
-=======
-            Application.Run(new Start());
->>>>>>> Stashed changes
         }
     }
 }
