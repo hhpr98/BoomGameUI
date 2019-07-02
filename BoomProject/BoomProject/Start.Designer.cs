7b435449a7ded728a91b11302031a9dd3d7f1b25
nguyenhuuhoa <nguyenhuuhoa1998@gmail.com>
2019-07-03 02:55:04 +0700

    revert 2 screen

---
+++ BoomProject/BoomProject/Start.Designer.cs
@@ -0,0 +1,243 @@
+﻿﻿namespace BoomProject
+{
+    partial class Start
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLine = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtColumn = new System.Windows.Forms.NumericUpDown();
+            this.txtCountBoom = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pbAvatar = new System.Windows.Forms.PictureBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.txtLine)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtColumn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtCountBoom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAvatar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(32, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kích thước";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(32, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Số boom";
+            // 
+            // txtLine
+            // 
+            this.txtLine.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLine.Location = new System.Drawing.Point(150, 114);
+            this.txtLine.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.txtLine.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.txtLine.Name = "txtLine";
+            this.txtLine.Size = new System.Drawing.Size(44, 22);
+            this.txtLine.TabIndex = 2;
+            this.txtLine.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(200, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "x";
+            // 
+            // txtColumn
+            // 
+            this.txtColumn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtColumn.Location = new System.Drawing.Point(220, 114);
+            this.txtColumn.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.txtColumn.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.txtColumn.Name = "txtColumn";
+            this.txtColumn.Size = new System.Drawing.Size(39, 22);
+            this.txtColumn.TabIndex = 4;
+            this.txtColumn.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // txtCountBoom
+            // 
+            this.txtCountBoom.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCountBoom.Location = new System.Drawing.Point(150, 152);
+            this.txtCountBoom.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.txtCountBoom.Name = "txtCountBoom";
+            this.txtCountBoom.Size = new System.Drawing.Size(44, 22);
+            this.txtCountBoom.TabIndex = 5;
+            this.txtCountBoom.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.MediumBlue;
+            this.label4.Location = new System.Drawing.Point(114, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(173, 31);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Boom Game";
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Blue;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Location = new System.Drawing.Point(20, 77);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(400, 4);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "label5";
+            // 
+            // pbAvatar
+            // 
+            this.pbAvatar.Location = new System.Drawing.Point(20, 12);
+            this.pbAvatar.Name = "pbAvatar";
+            this.pbAvatar.Size = new System.Drawing.Size(62, 50);
+            this.pbAvatar.TabIndex = 6;
+            this.pbAvatar.TabStop = false;
+            // 
+            // btnOK
+            // 
+            this.btnOK.BackColor = System.Drawing.Color.Crimson;
+            this.btnOK.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOK.ForeColor = System.Drawing.Color.White;
+            this.btnOK.Location = new System.Drawing.Point(95, 203);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(85, 31);
+            this.btnOK.TabIndex = 9;
+            this.btnOK.Text = "Xác nhận";
+            this.btnOK.UseVisualStyleBackColor = false;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.Crimson;
+            this.btnCancel.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.ForeColor = System.Drawing.Color.White;
+            this.btnCancel.Location = new System.Drawing.Point(220, 203);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(85, 31);
+            this.btnCancel.TabIndex = 10;
+            this.btnCancel.Text = "Thoát";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            // 
+            // Start
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(459, 260);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pbAvatar);
+            this.Controls.Add(this.txtCountBoom);
+            this.Controls.Add(this.txtColumn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtLine);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Start";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cấu hình";
+            this.Load += new System.EventHandler(this.Start_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.txtLine)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtColumn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtCountBoom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAvatar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown txtLine;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown txtColumn;
+        private System.Windows.Forms.NumericUpDown txtCountBoom;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pbAvatar;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
