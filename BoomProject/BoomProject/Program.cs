c83f486a9786a04173d94bc9246d4b07a6fcb569
nguyenhuuhoa <nguyenhuuhoa1998@gmail.com>
2019-07-03 02:26:35 +0700

    start screen

--- BoomProject/BoomProject/Program.cs
+++ BoomProject/BoomProject/Program.cs
@@ -16,7 +16,11 @@ namespace BoomProject
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+<<<<<<< Updated upstream
             Application.Run(new Form1());
+=======
+            Application.Run(new Start());
+>>>>>>> Stashed changes
         }
     }
 }
